ee961759ed2c51bb4f14ea725d252ba656c667ae
DonNguyen808 <DonNguyen808@gmail.com>
2021-09-19 11:16:59 -0400

    proper push

---
+++ 7kyu/busStops.js
@@ -0,0 +1,8 @@
+let number = function(busStops){
+    let totalPeople = 0;
+    for (let i=0; i<busStops.length; i++) {
+      totalPeople += busStops[i][0];
+      totalPeople -= busStops[i][1];
+    }
+    return totalPeople;
+  }

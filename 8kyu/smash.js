53e5226a639eca23b835507b20237cd00d36bb50
DonNguyen808 <DonNguyen808@gmail.com>
2021-10-12 10:37:43 -0400

    added 20th 8kyu

---
+++ 8kyu/smash.js
@@ -0,0 +1,8 @@
+function smash(words) {
+    return words.join(' ')
+};
+
+argument = ['Hi', 'my', 'name', 'is', 'Don']
+
+//expected output: Hi my name is Don
+console.log(smash(argument), 'Hi my name is Don')

c4edc40f1281a1cb9f0f65eb82406cbf8a76a2c1
DonNguyen808 <DonNguyen808@gmail.com>
2021-10-15 10:18:01 -0400

    Added 22nd 8kyu

---
+++ 8kyu/countSheep.js
@@ -0,0 +1,9 @@
+var countSheep = function (num){
+    let str = "";
+    for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+    return str;
+  }
+
+example = 8
+
+console.log(countSheep(example))

3fde6e5fe3bb05975f414c5f7c305fb69a12b40d
DonNguyen808 <DonNguyen808@gmail.com>
2021-12-01 23:06:01 -0500

    added 40th 7kyu

---
+++ 7kyu/addBinary.js
@@ -0,0 +1,13 @@
+function addBinary(a, b) {
+    return (a + b).toString(2)
+}
+
+//Expected output: -1000
+console.log(addBinary(-9, 1), -1000)
+
+//Expected output: 10
+console.log(addBinary(1, 1), 10)
+
+//Expected output: 1110
+console.log(addBinary(5, 9), 1110)
+

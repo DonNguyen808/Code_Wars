d077a39aa0feeb6c9170e251950dc232c861dc97
DonNguyen808 <DonNguyen808@gmail.com>
2021-12-13 19:33:42 -0500

    added 41th 7kyu

---
+++ 7kyu/SeriesSum.js
@@ -0,0 +1,15 @@
+function SeriesSum(n) {
+    for (var s = 0, i = 0; i < n; i++) {
+      s += 1 / (1 + i * 3)
+    }
+    
+    return s.toFixed(2)
+  }
+
+console.log(SeriesSum(1))
+console.log(SeriesSum(2))
+console.log(SeriesSum(5))
+
+//   1 --> 1 --> "1.00"
+//   2 --> 1 + 1/4 --> "1.25"
+//   5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"

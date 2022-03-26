723407e67e398ca0fcee971fe1f2b640303decf0
DonNguyen808 <DonNguyen808@gmail.com>
2022-03-26 00:11:18 -0400

    updated makeNegative 8kyu

--- 8kyu/makeNegative.js
+++ 8kyu/makeNegative.js
@@ -1,3 +1,18 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
 function makeNegative(num) {
-    return -Math.abs(num);
-  }
+    return -Math.abs(num)
+}
+
+function makeNegative2(num) {
+  return num < 0 ? num : -num;
+}
+
+
+
+console.log(makeNegative(1), -1)  
+console.log(makeNegative(-5),-5) 
+console.log(makeNegative(0),0)   
+console.log(makeNegative(0.12), -0.12)
+
+console.log(makeNegative2(1),-1)

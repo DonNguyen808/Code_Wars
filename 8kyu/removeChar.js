8b828ba9c180afd541f0edca721e0867b4b1ada0
DonNguyen808 <DonNguyen808@gmail.com>
2022-03-28 10:46:04 -0400

    updated removeChar

--- 8kyu/removeChar.js
+++ 8kyu/removeChar.js
@@ -1,3 +1,23 @@
-function removeChar(str) {
-    return str.slice(1, -1);
-  }
+function removeChar(str){
+   str1 = str.split('');
+   str1.shift();
+   str1.pop();
+   return str1.join('');
+     
+ };
+  
+testStr = 'Don'
+
+// expect result: o
+console.log(removeChar(testStr), 'o')
+  
+  
+function removeChar2(str) {
+  return str.slice(1, -1);
+}
+
+console.log(removeChar2('Dawn'))
+  
+
+
+

865f7e8eeb03582a09f0914620e864b4291486db
DonNguyen808 <DonNguyen808@gmail.com>
2022-03-30 18:13:08 -0400

    updated 8kyu summation

--- 8kyu/summation.js
+++ 8kyu/summation.js
@@ -1,7 +1,20 @@
-let summation = function (num) {
-    result = 0;
-    for (let i = 1; i <= num; i++){
-        result += i; 
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+
+var summation = function (num) {
+    result = 0
+    for (let i = 1; i <= num; i++) {
+        result += i
     }
-    return result;
+    return result
   }
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+console.log(summation(8))
+

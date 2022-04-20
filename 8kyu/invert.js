64784142e1c95b976277dc7fd301cc3986c2985d
DonNguyen808 <DonNguyen808@gmail.com>
2022-04-20 17:40:00 -0400

    Updated invert.js

--- 8kyu/invert.js
+++ 8kyu/invert.js
@@ -1,8 +1,17 @@
 // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
+// function invert(array) {
+//     return array.map(x => x === 0 ? x : -x);
+
+// }
+
 function invert(array) {
-    return array.map(x => x === 0 ? x : -x);
-}
+    let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(-array[i])
+    }
+    return newArr
+  }
 
 // [-1,-2,-3,-4,-5]
 console.log(invert([1,2,3,4,5]))

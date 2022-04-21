aad75b8441d24e17478ab3e99d1d5fde4081041b
DonNguyen808 <DonNguyen808@gmail.com>
2022-04-21 17:04:53 -0400

    Added 39th 8kyu

---
+++ 8kyu/reverseWords.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+
+
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' ')
+}
+
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+console.log(reverseWords("The greatest victory is that which requires no battle"))
+
+

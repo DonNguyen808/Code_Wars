68024840e133a0f63a1d32ca495a80f88d32fd78
DonNguyen808 <DonNguyen808@gmail.com>
2022-04-23 18:40:43 -0400

    Updated fakeBin.js

--- 8kyu/fakeBin.js
+++ 8kyu/fakeBin.js
@@ -2,11 +2,43 @@
 
 // Note: input will never be an empty string
 
+// function fakeBin(x) {
+//     let yArray = []
+//     xArray = x.split('')
+//     xArray.forEach(element => {
+//         if(element >= 5) {
+//             yArray.push('1')
+//         } else {
+//             yArray.push('0')
+//         }
+//     });
+//     return yArray.join('')
+// }
+
 function fakeBin(x) {
-    return x.split('').map(n => n < 5 ? 0 : 1).join('')
+    return x.split('').map(n => n < 5 ? '0' : '1').join('')
 }
 
 
 // expected results: '01'
 console.log(fakeBin('16'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

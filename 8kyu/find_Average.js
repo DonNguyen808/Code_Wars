b17a4f870a66210e871071a72061e27fff609280
DonNguyen808 <DonNguyen808@gmail.com>
2022-04-26 11:45:45 -0400

    Updated find_Average.js

--- 8kyu/find_Average.js
+++ 8kyu/find_Average.js
@@ -1,4 +1,25 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
 function find_average(array) {
-    var sum = array.reduce((a, b) => a + b, 0);
-    return sum/array.length;
+  if (array.length === 0) {
+    return 0
+  } else {
+    return array.reduce((a, b) => a + b, 0) / array.length 
   }
+}
+
+// function find_average(array) {
+//   return array.length === 0 ? 0 : array.reduce((a, b) => a + b, 0)/array.length
+// }
+
+// can do a for loop also
+
+
+console.log(find_average([]))
+console.log(find_average([20, 30]))
+
+
+
+

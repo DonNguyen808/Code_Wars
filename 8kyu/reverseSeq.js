699059c0c49c12f6f02e8bdf719593ea4cd4b871
DonNguyen808 <DonNguyen808@gmail.com>
2022-04-27 10:35:26 -0400

    Updated reverseSeq.js

--- 8kyu/reverseSeq.js
+++ 8kyu/reverseSeq.js
@@ -1,6 +1,21 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+
+
+
 const reverseSeq = n => {
-    let arr = [];
-      for (let i=n; i>0; i--) {
-        arr.push(i);
-        } return arr;
-    };
+  let arr = []
+  for (let i = n; i > 0; --i) {
+    arr.push(i)
+  }
+  return arr
+};
+
+// const reverseSeq = n => {
+//   return Array(n).fill(0).map((e, i) => n - i );
+// };
+
+// Example : n=5 --> [5,4,3,2,1]
+console.log(reverseSeq(5))
+
+

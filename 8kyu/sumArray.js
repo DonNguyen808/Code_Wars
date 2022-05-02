e109ea90db4d707a132356ba4813510fc232cd0f
DonNguyen808 <DonNguyen808@gmail.com>
2022-05-02 15:14:19 -0400

    updated sumArray.js

--- 8kyu/sumArray.js
+++ 8kyu/sumArray.js
@@ -1,14 +1,21 @@
-function sumArray(array) {
-    if (array == null) {
-      return 0;
-    } else if (array.length < 2) {
-      return 0;
-    } else {
-      array = array.sort(function(a,b) {return a - b;});
-      let total = 0;
-      for (let i = 1; i < array.length - 1; i++) {
-        total += array[i];
-      }
-      return total;
-    }
-  }
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+
+function sum (numbers) {
+
+  return numbers.reduce((acc, c) => acc + c, 0)
+} 
+
+
+// Output: 9.2
+console.log(sum([1, 5.2, 4, 0, -1]))
+
+// Output: 0
+console.log(sum([]))
+
+// Output: -2.398
+console.log(sum([-2.398]))
+
+
+
+

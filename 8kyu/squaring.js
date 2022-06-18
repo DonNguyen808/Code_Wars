34b1093b704e376b167149971f039d653804d4d8
DonNguyen808 <DonNguyen808@gmail.com>
2022-06-18 11:42:15 -0400

    added 81th 8kyu

---
+++ 8kyu/squaring.js
@@ -0,0 +1,16 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function squaring(n) {
+    return Math.pow(n, 2)
+}
+
+console.log(squaring(3), 9)
+console.log(squaring(4), 16)
+console.log(squaring(5), 25)
+
+// PREP
+// parameter a integer
+// return squared
+// example 4 = 16
+// pseudo
+// math method

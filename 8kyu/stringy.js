a2789441ab9f95ac67090d60ae32479b6188b117
DonNguyen808 <DonNguyen808@gmail.com>
2022-07-10 22:15:03 -0400

    Added PREP to stringy.js

--- 8kyu/stringy.js
+++ 8kyu/stringy.js
@@ -10,17 +10,41 @@
 
 // The size will always be positive and will only use whole numbers.
 
+
+
+/* 
+PREP
+Parameter:
+    It will always start with 1 followed by 0 repeating and stops on the given size. Always a positive number
+
+Return:
+    return 1 and 0 until it reaches the size
+
+Example:
+    stringy(5) => 10101
+    stringy(2) => 10
+    stringy(1) => 1
+
+Pseudo Code:
+    create a variable to store the value
+    make a for loop and add 1 and 0 when its a odd or even number
+*/
+
 function stringy(size) {
     let str = ''
-    for(let i = 1; i <= size; i++) {
+    for (let i = 1; i <= size; i++) {
         str += i % 2
     }
     return str
+
 }
 
-console.log(stringy(4), '1010');
-console.log(stringy(5), '10101');
-console.log(stringy(2), '10');
+console.log(stringy(2), 10)
+console.log(stringy(5), 10101)
+console.log(stringy(1), 1)
+
+
+
 
 
 

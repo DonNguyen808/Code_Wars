a2ce80c76db90c78ae9b1b69e37155f2939386b2
DonNguyen808 <DonNguyen808@gmail.com>
2022-07-18 21:46:50 -0400

    Added PREP to oddCount.js

--- 8kyu/oddCount.js
+++ 8kyu/oddCount.js
@@ -5,9 +5,40 @@
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 // Expect large Inputs!
 
-const oddCount = n => Math.floor(n / 2)
 
-console.log(oddCount(7), 3)
-console.log(oddCount(15023), 7511)
+/* PREP
+Parameter:
+    given a integer, it can be positive or negative
+    can it accept empty, null, strings?
+
+Return:
+    return the number of times it takes a odd number to reach the given value
+
+Example:
+    oddCount(7) => 4 because [1, 3, 5, 7]
+    oddCount(10) => 5 because [1, 3, 5, 7, 9]
+
+Pseudo Code:
+    take the given integer and divide it by 2 which will give us how many odd numbers
+    then use the Math floor method to round down
+
+
+ */
+
+function oddCount(n) {
+    return Math.floor(n / 2)
+}
+
+console.log(oddCount(7))
+console.log(oddCount(15))
+console.log(oddCount(10))
+console.log(oddCount(15023))
+console.log(oddCount(1184.3))
+
+
+
+
+
+
 
 

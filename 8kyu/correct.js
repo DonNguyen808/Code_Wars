4ad05be6ae5a8d44cce58283239df000ac05f1cf
DonNguyen808 <DonNguyen808@gmail.com>
2022-07-25 20:42:35 -0400

    Updated correct.js

--- 8kyu/correct.js
+++ 8kyu/correct.js
@@ -33,10 +33,27 @@ use regular expression to replace
 
 */
 
-correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+function correct(str) {
+    // return str.includes('0')
+    return str.replaceAll('0', 'O')
+}
 
 console.log(correct("L0ND0N"),"LONDON");
 console.log(correct("DUBL1N"),"DUBLIN");
 console.log(correct("51NGAP0RE"),"SINGAPORE");
 console.log(correct("BUDAPE5T"),"BUDAPEST");
 console.log(correct("PAR15"),"PARIS");
+
+
+
+
+
+
+
+
+
+
+
+
+// correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+

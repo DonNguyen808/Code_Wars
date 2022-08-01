54fbc5bac147c5bc36cfa476237fd2ab2e05b93e
DonNguyen808 <DonNguyen808@gmail.com>
2022-07-31 21:52:51 -0400

    add getRealFloor.js

--- 8kyu/getRealFloor.js
+++ 8kyu/getRealFloor.js
@@ -6,17 +6,43 @@
 
 // Basements (negatives) stay the same as the universal level.
 
-// Examples
+
+// Example
 // 1  =>  0 
 // 0  =>  0
 // 5  =>  4
 // 15  =>  13
 // -3  =>  -3
 
+/* PREP
+Parameter: given a number, it can be negative
+Return: starts at 0 instead of 1 therefore will be 1 difference and skips 13, but if its negative it remains unchanged
+example: 0 => 1
+    13 => 11
+    -1 => -1
+Pseudo Code: make a check on the number whether its positive or negative and if its higher than 13
+    return the number
+
+*/
+
+testCase = 5
+testCase2 = -3
+
 function getRealFloor(n) {
     return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
-}
+  }
 
-console.log(getRealFloor(1), 0);
-console.log(getRealFloor(5), 4);
-console.log(getRealFloor(15), 13);
+
+
+// function getRealFloor(n) {
+//     if (Math.sign(n) == 1 && n >= 13) {
+//         return n - 2
+//     } else if (Math.sign(n) == 1) {
+//         return n - 1
+//     } else {
+//         return n
+//     }
+// }
+
+console.log(getRealFloor(testCase))
+console.log(getRealFloor(testCase2))

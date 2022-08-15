ae3a40d0692851493af3dd050b68b3db52a1f9dd
DonNguyen808 <DonNguyen808@gmail.com>
2022-08-15 01:12:15 -0400

    updated removeExclamationMarks.js

--- 8kyu/removeExclamationMarks.js
+++ 8kyu/removeExclamationMarks.js
@@ -1,16 +1,17 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
-// Regular Expression
+
+/* 
+P: given a string with !
+R: return back the value without the !
+E: 'keep! => 'keep'
+P: split character into a array, split at ! and join back together
+*/
+
 function removeExclamationMarks(s) {
-    return s.replace(/!/gi, '');
+    return s.split('!').join('')
 }
 
-// function removeExclamationMarks(s) {
-//     return s.split('!').join('');
-// }
-
-
-console.log(removeExclamationMarks("Hello World!!", "Hello World"))
-
+console.log(removeExclamationMarks("Hello World!"), "Hello World")
 
 

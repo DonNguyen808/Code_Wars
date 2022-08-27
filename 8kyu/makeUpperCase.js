b00c7c21f84523c85f80d7cd71d3ed53fe4c8933
DonNguyen808 <DonNguyen808@gmail.com>
2022-08-26 21:23:57 -0400

    Improved makeUppercase.js

--- 8kyu/makeUpperCase.js
+++ 8kyu/makeUpperCase.js
@@ -1,3 +1,27 @@
-const makeUpperCase = str => str.toUpperCase();
+// Write a function which converts the input string to uppercase.
+
+// function makeUpperCase(str) {
+//     return str.toUpperCase()
+// }
+
+let makeUpperCase = str => str.toUpperCase()
+
+console.log(makeUpperCase('hello'), 'HELLO')
+console.log(makeUpperCase('world'), 'WORLD')
+console.log(makeUpperCase('everyone'), 'EVERYONE')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

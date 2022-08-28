0ef4a68d050f1c08be4c074bed4ffcfe6e49db75
DonNguyen808 <DonNguyen808@gmail.com>
2022-08-27 22:01:09 -0400

    Updating findNeedle.js

--- 8kyu/findNeedle.js
+++ 8kyu/findNeedle.js
@@ -1,3 +1,16 @@
-function findNeedle(haystack) {
-    return "found the needle at position " + haystack.indexOf("needle");
-  }
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+
+// function findNeedle(haystack) {
+//     return "found the needle at position " + haystack.indexOf("needle");
+//   }
+
+let findNeedle = haystack => `found the needle at position ${haystack.indexOf("needle")}`;
+
+console.log(findNeedle(['hay', 'hay', 'needle']))
+console.log(findNeedle(['hay', 'hay', 'hay']))

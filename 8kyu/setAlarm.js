67b2b488d3e0d199f783c5ae343b8e8baedb39ed
DonNguyen808 <DonNguyen808@gmail.com>
2022-08-29 15:38:37 -0400

    v2 update

--- 8kyu/setAlarm.js
+++ 8kyu/setAlarm.js
@@ -9,25 +9,16 @@
 //     }
 // }
 
-function setAlarm(employed, vacation) {
-    return employed && !vacation;
-}
+// function setAlarm(employed, vacation) {
+//     return employed && !vacation;
+// }
+
+const setAlarm = (employed, vacation) => employed && !vacation
 
 console.log(setAlarm(true, false), true)
 console.log(setAlarm(false, false), false)
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(setAlarm(true, true), false)
+console.log(setAlarm(false, true), false)
 
 
 

023da11b1124bc8c6f523ba5c9d9e10f0b20659a
DonNguyen808 <DonNguyen808@gmail.com>
2022-08-30 17:09:51 -0400

    update v1

--- 8kyu/grow.js
+++ 8kyu/grow.js
@@ -1,6 +1,31 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// function grow(x) {
+//     return x.reduce((a, b) => a * b)
+// }
+
 const grow = x => x.reduce((a,b) => a * b);
 
-test = grow([1,2,3])
+let test = grow([1,2,3])
 
-// 1 * 2 * 3 = 6
-console.log(test)
+console.log(test, 6)
+console.log(grow([4,6,7]), 168)
+console.log(grow([10,329,1]), 3290)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

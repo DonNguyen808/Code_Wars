a89ad75bfe97834f5c1bdd03007ee63773298e31
Don Nguyen <donnguyen808@gmail.com>
2024-06-18 22:15:05 -0400

    added more solutions

--- 8kyu/shortcut.js
+++ 8kyu/shortcut.js
@@ -8,11 +8,45 @@
 // don't worry about uppercase vowels
 // y is not considered a vowel for this kata
 
-function shortcut(str) {
-    return str.split('').filter(function(e) {
-      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
-    }).join('')
-  }
+// function shortcut(str) {
+//     return str.split('').filter(function(e) {
+//       return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+//     }).join('')
+//   }
+
+function shortcut(string) {
+  let vowelList = ['a', 'e', 'i', 'o', 'u']
+
+  return string.split('').filter(e => 
+    vowelList.indexOf(e) == -1
+  ).join('')
+}
+
+
+
+// failed on some unknown test cases
+// function shortcut(string) {
+//   let vowelList = ['a', 'e', 'i', 'o', 'u']
+
+//   let stringList = string.split('')
+
+//   let result = []
+
+//   for (let i = 0; i < stringList.length; i++) {
+//     if (!vowelList.includes(stringList[i]))
+//       result += stringList[i]
+//   }
+//     return result
+
+// }
+
+
+// function shortcut(string){
+//   return string.replace(/[aeiou]/g,'')
+// }
+
+
+
 
 console.log(shortcut('hello'), 'hll');
 console.log(shortcut('how are you today?'), 'hw r y tdy?');

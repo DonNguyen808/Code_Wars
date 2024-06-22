3e324cbd917bec1f40bd2bbdc66c450b09f1b472
Don Nguyen <donnguyen808@gmail.com>
2024-06-21 23:34:37 -0400

    updated sumArray2.js

--- 8kyu/sumArray2.js
+++ 8kyu/sumArray2.js
@@ -1,34 +1,27 @@
-// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+/* 
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 
-// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
-// Mind the input validation.
+Mind the input validation.
 
-// Input validation
-// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
-
-// function sumArray2(array) {
-//     if (array == null || array < 2) return 0
-//     let total = 0
-//     let sorted = array.sort((a,b) => a - b)
-//     for (let i = 1; i < sorted.length - 1; i++) {
-//         total += sorted[i]
-//     }
-//     return total;
-// }
-
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+*/
 
 function sumArray2(arr) {
     if (arr == null || arr < 2) return 0
-    let exceptionNums = Math.min(...arr) + Math.max(...arr)
     let total = 0
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i]
+    let sorted = arr.sort((a,b) => a - b)
+    for (let i = 1; i < sorted.length - 1; i++) {
+        total += sorted[i]
     }
-    return total - exceptionNums
+    return total;
 }
-
-
 
 console.log(sumArray2(null), 0 );
 console.log(sumArray2([ ]), 0 );

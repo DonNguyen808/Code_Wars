43b8410adcfd024b3cbe308fcc9d6b7927ef5ad8
Don Nguyen <donnguyen808@gmail.com>
2024-06-25 23:59:50 -0400

    first commit

---
+++ 8kyu/remove.js
@@ -0,0 +1,32 @@
+/* 
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+*/
+
+/* 
+string containing exclamation marks
+return back the string without the exclamation marks
+hi! => hi
+loop through and check if its a exclamation mark, if it is then remove it, if it isnt, do nothing
+
+*/
+
+function remove (string) {
+    return 0
+
+}
+
+console.log("Hi!", "Hi");
+console.log("Hi!!!", "Hi!!");
+console.log("!Hi", "!Hi");
+console.log("!Hi!", "!Hi");
+console.log("Hi! Hi!", "Hi! Hi");
+console.log("Hi", "Hi");
